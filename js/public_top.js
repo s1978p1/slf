73bbe483b9344ee0cfc33e0c0de310d207f6d876
yz <17771799935@163.com>
2020-10-28 13:40:44 +0800

    10th

--- js/public_top.js
+++ js/public_top.js
@@ -8,15 +8,15 @@ $().ready(function(){
 	// 导航栏底部线条移动动画结束
 	
 	//顶部位置判断
-	$(window).scroll(function() {
-        var t_Head = $(".public_head").find("h1").height();
-        var y = $(window).scrollTop();
-        if (y < t_Head) {
-        	$(".public_head").find("h1").css("position","static");
-        }else{
-        	$(".public_head").find("h1").css("position","fixed");
-        }
-    }); 
+	// $(window).scroll(function() {
+    //     var t_Head = $(".public_head").find("h1").height();
+    //     var y = $(window).scrollTop();
+    //     if (y < t_Head) {
+    //     	$(".public_head").find("h1").css("position","static");
+    //     }else{
+    //     	$(".public_head").find("h1").css("position","fixed");
+    //     }
+    // }); 
 	//顶部位置判断结束
 
 	// 汉堡包点击事件
